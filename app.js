9eed28916e5be70426b8da659a53d52317de869a
lakshay3749 <lakshayyadav112414@gmail.com>
2026-04-07 22:34:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-// const path = require('path')
+
+const {Server} = require('socket.io')
+const http = require('http')
+
 const cors = require('cors')
 const { default: mongoose } = require('mongoose');
 const { homeRoute } = require('./routes/homeRouter');
 const { passRoute } = require('./routes/passRouter');
 const { vehicleRoute } = require('./routes/vehicleRoute');
+const { residentRoute } = require('./routes/residentRouter');
+const { initSocket } = require('./socket')
+const { guardRoute } = require('./routes/guardRouter')
 
-const app = express();
+
+
+const app = express();  
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
 app.use(cors());
 
-app.use(vehicleRoute)
+// app.use(vehicleRoute)
+// app.use(homeRoute)  
 
-app.use(homeRoute)  
-console.log("Hello Saint")
+
 app.use(passRoute);
+
+app.use(residentRoute) // opening resident route 
+
+app.use('/guard',guardRoute)
+
 
 
 
@@ -30,7 +43,12 @@ const PORT = process.env.PORT || 3000
 mongoose.connect(DB_PATH).then(()=>{
     console.log("Connection to Mongo Successfull...")
 
-     app.listen(PORT, "0.0.0.0", (err) => {
+    const server = http.createServer(app)
+
+    initSocket(server);
+
+
+     server.listen(PORT, "0.0.0.0", (err) => {
        console.log("Server is running ...")
      })
 
