1bc85df852395b0e127ef6433529b5855eef7083
lakshay3749 <lakshayyadav112414@gmail.com>
2026-04-07 21:11:07 +0530

    Add files via upload

--- routes/passRouter.js
+++ routes/passRouter.js
@@ -1,10 +1,29 @@
 const express = require('express');
-const { passController, addResidentVehicleController } = require('../controller/passController');
+// const fetch = require("node-fetch");
+const axios = require("axios")
+
+const multer = require("multer")
+
+const cloudinary = require('cloudinary').v2
+
+const WORKER_URL = "http://localhost:8787"; 
+
+const { passController, addResidentVehicleController, staffPassController, staffPassHistoryController, addNewPersonalStaffController, fetchAllPersonalStaffController, updatePersonalStaffScheduleController, updateEntryAllowedController, updateStaffAttendanceController, updateStaffPhotoController } = require('../controller/passController');
 const { verifyPassController } = require('../controller/passController');
 
 const passRoute = express.Router();
 
 
+
+cloudinary.config({
+  cloud_name: "dd5pdy82n",
+  api_key: "397341551664711",
+  api_secret: "G8Erp1raivFJyMChleae3nOXwpI"
+});
+
+// G8Erp1raivFJyMChleae3nOXwpI
+// 397341551664711
+// dd5pdy82n
 
 
 passRoute.post('/addVisitorPass',passController);
@@ -12,15 +31,113 @@ passRoute.post('/addVisitorPass',passController);
 passRoute.post('/verifyVisitorPass',verifyPassController)
 
 passRoute.post('/addResidentVehicles',(req,res,next)=>{
-    console.log(req.body,"boli")
+    console.log(req.body,"bolid")
     res.status(200).json({ message: "Data received successfully!"})
     
 
 })
 
-passRoute.get('./hello',(req,res,next)=>{
-    console.log(req.body)
+passRoute.post('/addStaffPass',staffPassController)
+
+passRoute.get('/fetchStaffHistory',staffPassHistoryController)
+
+
+passRoute.post('/addPersonalStaff',addNewPersonalStaffController)
+
+passRoute.get('/fetchAllPersonalStaff',fetchAllPersonalStaffController)
+
+passRoute.post('/updatePersonalStaffSchedule',updatePersonalStaffScheduleController)
+
+passRoute.post('/updateEntryAllowedStatus',updateEntryAllowedController)
+
+passRoute.post('/addfaceembedding2server',async (req,res,next)=>{
+    try {
+    const { workerId, vector } = req.body;
+
+
+   console.log("Hit sending")
+    const response = await axios.post(
+      "https://face-worker.lakshay9763.workers.dev/insert", // your worker URL
+      {
+        id: workerId,
+        vector: vector,
+      }
+    );
+
+    // const response = await axios.post(
+    //   "https://face-worker.lakshay9763.workers.dev/search",
+    //   { vector }
+    // );
+
+    console.log(response.data,'evil ...!')
+
+    res.json(response.data);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: "Insert failed" });
+  }
 })
 
+
+passRoute.post('/comparefaceembedding',async (req,res,next)=>{
+    try {
+    const { vector } = req.body;
+
+
+
+   
+    const response = await axios.post(
+      "https://face-worker.lakshay9763.workers.dev/search",
+      { vector }
+    );
+
+    console.log('evil ...!')
+
+    res.json(response.data);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: "Insert failed" });
+  }
+})
+
+
+passRoute.post('/updateStaffAttendance',updateStaffAttendanceController)
+
+
+
+const upload = multer({dest:"uploads/"})
+
+passRoute.post('/updateStaffPhoto',upload.single("image"), updateStaffPhotoController)
+
+
+
+
+
+
+// {
+//   workerId: 'WRK-MMPXUIEC',
+//   residentId: 'RES-B204',
+//   category: 'maid',
+//   since: 'Mar 2026',
+//   entryAllowed: true,
+//   status: 'active',
+//   name: 'Sunita',
+//   phone: '55802580',
+//   salary: '500',
+//   shift: 'All day',
+//   schedule: {
+//     Mon: true,
+//     Tue: true,
+//     Wed: false,
+//     Thu: true,
+//     Fri: false,
+//     Sat: true,
+//     Sun: false
+//   },
+//   tasks: [ 'Cleaning' ],
+//   photo: null
+// }
 
 exports.passRoute = passRoute
