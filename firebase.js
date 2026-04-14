396da8938973075f5b622d780eb3942608776b04
lakshay3749 <lakshayyadav112414@gmail.com>
2026-04-14 20:34:32 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@
 const admin = require('firebase-admin');
 
 // Load your service account key JSON file
-const serviceAccount = require('./googleServices.json'); 
+const googleServices = JSON.parse(process.env.GOOGLE_SERVICES_JSON);
 // Make sure this file is downloaded from Firebase Console → Project Settings → Service Accounts
 
 // Initialize the Firebase Admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(googleServices),
   databaseURL: "https://<your-project-id>.firebaseio.com" // replace with your Firebase project URL
 });
 
